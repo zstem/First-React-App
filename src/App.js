176a72b46a264791cf36733a5336b651a37638dc
Zack <zackstempowicz@gmail.com>
2022-01-16 18:27:54 -0500

    first commit

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import logo from './zs.jpg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        
+        <a
+          className="App-link"
+          href="https://github.com/zstem"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Zack
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
